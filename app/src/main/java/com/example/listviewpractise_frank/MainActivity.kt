bdda74ce5b24585b5a4d7b16acd8ff128bd4f53f
FrankinKor <tgifrank32@gmail.com>
2023-08-07 23:20:42 +0900

    학생목록 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/example/listviewpractise_frank/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractise_frank/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.listviewpractise_frank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractise_frank.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student> ()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("코딩디처지니", 1988))
+        mStudentList.add(Student("남궁민", 2000))
+        mStudentList.add(Student("김용수", 1900))
+        mStudentList.add(Student("원빈초이", 1800))
+        mStudentList.add(Student("성빈파크", 1850))
+        mStudentList.add(Student("형샘킴", 1899))
+        mStudentList.add(Student("현성김", 1901))
+        mStudentList.add(Student("성민쩐", 1956))
+
+
+
+
     }
 }
--- app/src/main/java/com/example/listviewpractise_frank/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractise_frank/adapters/StudentAdapter.kt
@@ -1,11 +1,33 @@
 package com.example.listviewpractise_frank.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractise_frank.R
 import com.example.listviewpractise_frank.datas.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+
+        val inf = LayoutInflater.from(mContext)
+리
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return  row
+    }
+
+
 }
